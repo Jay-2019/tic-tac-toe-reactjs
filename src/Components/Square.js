d9744eeea875fd074ff7c1f6d066cc256603dad5
Jay Mauryavanshi <47723510+Jay-2019@users.noreply.github.com>
2019-11-28 15:58:32 +0530

    Update Square.js

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,18 +1,13 @@
 import React from "react";
 import style from "../style.module.css";
-
-// square component
-class Square extends React.Component {
-
-    render() {
-        return (
-            <button
-                className={style.square}
-                onClick={this.props.onClick}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+const Square = (props) => {
+    return (
+        <button
+            className={style.square}
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
 }
 export default Square;
